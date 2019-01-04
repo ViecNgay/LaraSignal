4899087a641f687bd6709f6118dc0a8fe9a92c5f
Quyet Tran <quyettv.it@gmail.com>
2019-01-04 16:15:08 +0700

    update LaraSignal With additional Options

--- src/LaraSignal.php
+++ src/LaraSignal.php
@@ -27,33 +27,34 @@ class LaraSignal
         }
         $deviceIds = (array) $deviceIds;
         $client = new \GuzzleHttp\Client();
-
+        $json = [
+            "app_id" => config('larasignal.one_signal_app_id'),
+            "contents" => [
+                "en" => $subTitle,
+                "vi" => $subTitle
+            ],
+            "headings" => [
+                "en" => $title,
+                "vi" =>  $title
+            ],
+            "data" => $payload,
+            "include_player_ids" => $deviceIds,
+            "web_url" => $url['web_url'],
+            "app_url" => $url['app_url'],
+            "android_led_color" => "FF0000FF",
+            "big_picture" => $imageUrl,
+            "chrome_big_picture" => $imageUrl
+        ];
+        $json = array_merge($json, $options);
         $data = [
             'headers' => [
                 'Authorization' => 'Basic ' . config('larasignal.one_signal_rest_api_key'),
                 'Content-Type'     => 'application/json',
             ],
-            'json' => [
-                "app_id" => config('larasignal.one_signal_app_id'),
-                "contents" => [
-                    "en" => $subTitle,
-                    "vi" => $subTitle
-                ],
-                "headings" => [
-                    "en" => $title,
-                    "vi" =>  $title
-                ],
-                "data" => $payload,
-                "include_player_ids" => $deviceIds,
-                "web_url" => $url['web_url'],
-                "app_url" => $url['app_url'],
-                "android_led_color" => "FF0000FF",
-                "big_picture" => $imageUrl,
-                "chrome_big_picture" => $imageUrl
-            ]
+            'json' => $json
         ];
 
-        $data = array_merge($data, $options);
+
         $res = $client->request('POST', $this->createNotificationUrl, $data);
         return true;
     }
@@ -78,32 +79,33 @@ class LaraSignal
         $segments = (array) $segments;
         $client = new \GuzzleHttp\Client();
 
+        $json = [
+            "app_id" => config('larasignal.one_signal_app_id'),
+            "contents" => [
+                "en" => $subTitle,
+                "vi" => $subTitle
+            ],
+            "headings" => [
+                "en" => $title,
+                "vi" =>  $title
+            ],
+            "data" => $payload,
+            "include_player_ids" => $deviceIds,
+            "web_url" => $url['web_url'],
+            "app_url" => $url['app_url'],
+            "android_led_color" => "FF0000FF",
+            "big_picture" => $imageUrl,
+            "chrome_big_picture" => $imageUrl
+        ];
+        $json = array_merge($json, $options);
         $data = [
             'headers' => [
                 'Authorization' => 'Basic ' . config('larasignal.one_signal_rest_api_key'),
                 'Content-Type'     => 'application/json',
             ],
-            'json' => [
-                "app_id" => config('larasignal.one_signal_app_id'),
-                "contents" => [
-                    "en" => $subTitle,
-                    "vi" => $subTitle
-                ],
-                "headings" => [
-                    "en" => $title,
-                    "vi" =>  $title
-                ],
-                "data" => $payload,
-                "include_player_ids" => $deviceIds,
-                "web_url" => $url['web_url'],
-                "app_url" => $url['app_url'],
-                "android_led_color" => "FF0000FF",
-                "big_picture" => $imageUrl,
-                "chrome_big_picture" => $imageUrl
-            ]
+            'json' => $json
         ];
 
-        $data = array_merge($data, $options);
         $res = $client->request('POST', $this->createNotificationUrl, $data);
         return true;
     }
